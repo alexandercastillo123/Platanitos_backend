d6161f29b4e411a4c237dc11aaf0e637cd3184a9
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-05-30 11:17:44 -0500

    feat: estructura backend

---
+++ src/main/java/com/api/platanitos/PlatanitosApplication.java
@@ -0,0 +1,13 @@
+package com.api.platanitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatanitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatanitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/platanitos/PlatanitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.platanitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatanitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
