934e2abeb6595e58b5c45c118f26b7bda46bc808
BytheasPls <leovretrl@gmail.com>
2026-06-01 07:03:35 -0500

    Commit de prueba

--- src/main/java/com/api/platanitos/models/CodigoVerificacion.java
+++ src/main/java/com/api/platanitos/models/CodigoVerificacion.java
@@ -1,5 +1,64 @@
 package com.api.platanitos.models;
 
-public class codigoVerificacion {
+import java.time.LocalDateTime;
 
+import com.api.platanitos.enums.TipoToken;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ToString(exclude = "usuario")
+@Table(name = "codigo_verificacion")
+public class CodigoVerificacion extends EntidadBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String token;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoToken tipo;
+    @Column(name = "fecha_expiracion")
+    private LocalDateTime fechaExpiracion;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CodigoVerificacion that = (CodigoVerificacion) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    public boolean isExpirado(){
+        return LocalDateTime.now().isAfter(this.fechaExpiracion);
+    }
 }
